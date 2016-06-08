280728997aaaba6ee65284e3903f76a483c04770
Matt Gruskin <matthew.gruskin@gmail.com>
2016-06-08 10:12:08 -0400

    add a test for the login action creator

---
+++ test/login_actions_spec.js
@@ -0,0 +1,101 @@
+import { assert } from 'chai';
+import { fromJS } from 'immutable';
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+import * as sinon from 'sinon';
+
+import { stubFetch } from './test_utils';
+
+import * as actions from '../src/actions/login_actions';
+import { PERMISSIONS } from '../src/util/user';
+
+const mockStore = configureMockStore([thunkMiddleware]);
+
+describe('login action creator', () => {
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('login flow', () => {
+    it('dispatches action on successful login', done => {
+      const store = mockStore(fromJS({
+        auth: {
+          username: 'admin',
+          password: 'adminpassword',
+        },
+      }));
+      const router = [];
+
+      const fetches = {
+        '/users/admin': {
+          username: 'admin',
+        },
+      };
+      PERMISSIONS.forEach(permission => {
+        fetches[`/authorized?permission=${permission}`] = {
+          authorized: true,
+        };
+      });
+      stubFetch(sandbox, fetches);
+
+      store.dispatch(actions.login(router))
+        .then(() => {
+          assert.includeDeepMembers(store.getActions(), [
+            {
+              type: 'AUTH_LOGIN',
+            },
+            {
+              type: 'AUTH_LOGIN_DONE',
+              user: {
+                username: 'admin',
+              },
+              permissions: PERMISSIONS,
+            },
+          ]);
+          assert.deepEqual(router, ['/']);
+          done();
+        })
+        .catch(done);
+    });
+    it('dispatches action on failed login', done => {
+      const store = mockStore(fromJS({
+        auth: {
+          username: 'admin',
+          password: 'adminpassword',
+        },
+      }));
+      const router = [];
+
+      stubFetch(sandbox, {
+        '/users/admin': {
+          code: 401,
+          description: 'Unauthorized',
+        },
+      });
+
+      store.dispatch(actions.login(router))
+        .then(() => {
+          assert.includeDeepMembers(store.getActions(), [
+            {
+              type: 'AUTH_LOGIN',
+            },
+            {
+              type: 'AUTH_LOGIN_DONE',
+              error: {
+                statusCode: 401,
+                statusText: 'Unauthorized',
+                description: 'Unauthorized',
+              },
+            },
+          ]);
+          assert.deepEqual(router, []);
+          done();
+        })
+        .catch(done);
+    });
+  });
+});
